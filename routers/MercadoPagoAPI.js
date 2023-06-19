0517be3cadef9d968438d20f1e214f028830fa25
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-06-19 18:16:09 -0300

    fix mp api

--- routers/MercadoPagoAPI.js
+++ routers/MercadoPagoAPI.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const router = express.Router();
 require('dotenv').config();
 // SDK de Mercado Pago
 const mercadopago = require("mercadopago");
@@ -7,6 +9,7 @@ mercadopago.configure({
 });
 
 // Crear un objeto de preferencia
+router.post('/crear-preferencia', (req, res) => {
     let preference = {
     // el "purpose": "wallet_purchase" solo permite pagos registrados
     // para permitir pagos de guests puede omitir esta propiedad
@@ -25,6 +28,11 @@ mercadopago.configure({
     .then(function (response) {
       // Este valor es el ID de preferencia que se enviará al ladrillo al inicio
       const preferenceId = response.body.id;
+      res.json({preferenceId});
     }).catch(function (error) {
       console.log(error);
+      res.status(500).json({ error: 'Error al crear la preferencia' });
     });
+  });
+
+  module.exports = router;
