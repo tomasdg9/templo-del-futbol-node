02335e08449c0df0adef355371e4920f5855532e
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-06-20 22:26:46 -0300

    mp

--- routers/MercadoPagoAPI.js
+++ routers/MercadoPagoAPI.js
@@ -1,11 +1,59 @@
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
+const axios = require('axios');
+
 // SDK de Mercado Pago
 const mercadopago = require("mercadopago");
 // Agrega credenciales
 mercadopago.configure({
   access_token: process.env.ACCESS_TOKEN,
+});
+
+// Ruta para procesar el pago con tarjeta
+router.post('/process_payment', (req, res) => {
+  const paymentData = {
+    transaction_amount: req.body.amount,
+    // Resto de los datos del pago
+  };
+
+  axios.post('https://api.mercadopago.com/v1/payments', paymentData, {
+    headers: {
+      Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+    },
+  })
+    .then(function(response) {
+      const { status, status_detail, id } = response.data;
+      res.status(response.status).json({ status, status_detail, id });
+    })
+    .catch(function(error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error en el pago' });
+    });
+});
+
+router.get('/obtener-preference-id', (req, res) => {
+  // Crea la preferencia de pago
+  const preferenceData = {
+    items: [
+      {
+        title: 'Producto de ejemplo',
+        quantity: 1,
+        currency_id: 'ARS',
+        unit_price: 100,
+      },
+    ],
+  };
+
+  mercadopago.preferences.create(preferenceData)
+    .then((response) => {
+      const preferenceId = response.body.id;
+      res.json({ preferenceId });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: 'Error al obtener el preferenceId' });
+    });
 });
 
 // Crear un objeto de preferencia
